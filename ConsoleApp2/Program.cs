e9fc084fef48f9143370354daf069df69d1a4d71
Brionna Franklin <s198017@students.aie.edu>
2019-08-21 17:53:53 -0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //select sign
+            Console.WriteLine("+, -, *, or /?");
+            Console.ReadLine();
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+            Console.WriteLine(NotFiniteNumberException);
+            Console.ReadKey()
+
+            if ( = "+")
+            {
+                Add();
+            }
+            else if ( = "-")
+            {
+                Subtract();
+            }
+            else if ( = "*")
+            {
+                Multiply();
+            }
+            else if ( + "/")
+            {
+                Divide();
+            }
+
+            bool Add()
+            {
+
+            }
+            bool Subtract()
+            {
+
+            }
+            bool Multiply()
+            {
+
+            }
+            bool Divide()
+            {
+
+            }
+        }
+    }
+}
